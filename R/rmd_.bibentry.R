987df867fe2993969eb879e4231702c73f310d77
Tingting Zhan <tingtingzhan@gmail.com>
2024-09-11 09:57:25 -0400

    test

--- R/rmd_.bibentry.R
+++ R/rmd_.bibentry.R
@@ -5,11 +5,12 @@
 #' @description 
 #' R markdown format of a \link[utils]{citation} and/or \link[utils]{bibentry} object.
 #' 
-#' @param x \link[base]{character} scalar, 
-#' `'R'` (default) or name of an R package
+#' @param x a \link[utils]{citation} and/or \link[utils]{bibentry} object
+#'
+#' @param ... additional parameters, currently not in use
 #' 
 #' @details
-#' Function [bibentry2rmd] beautifies the output from 
+#' Function [rmd_.bibentry] beautifies the output from 
 #' function `utils:::format.bibentry` (with option `style = 'text'`)
 #' in the following ways.
 #' \itemize{
@@ -19,25 +20,21 @@
 #' }
 #' 
 #' @returns 
-#' Function [bibentry2rmd] returns a \link[base]{character} scalar or \link[base]{vector}.
+#' Function [rmd_.bibentry] returns a \link[base]{character} scalar or \link[base]{vector}.
 #' 
 #' @examples 
-#' bibentry2rmd('survival')
+#' rmd_.bibentry(citation())
+#' rmd_.bibentry(citation('survival'))
 #' if (FALSE) { # disabled for ?devtools::check
 #' ap = rownames(installed.packages())
-#' lapply(ap, FUN = bibentry2rmd)
+#' lapply(ap, FUN = function(i) rmd_.bibentry(citation(i)))
 #' }
 #' @importFrom stringi stri_extract_all_regex stri_replace_all_fixed stri_replace_all_regex
-#' @importFrom utils citation
+#' @export rmd_.bibentry
 #' @export
-bibentry2rmd <- function(x = 'R') {
+rmd_.bibentry <- function(x, ...) {
   
-  if (length(x) != 1L || !is.character(x)) stop('`x` must be len-1 character')
-  if (is.na(x) || !nzchar(x)) stop('`x` must not be missing')
-    
-  x0 <- switch(x, R = citation(), suppressWarnings(citation(package = x))) # c('citation', 'bibentry')
-  
-  y <- format(x0, style = 'text') # ?utils:::format.citation -> ?utils:::format.bibentry
+  y <- format(x, style = 'text') # ?utils:::format.citation -> ?utils:::format.bibentry
   # return may have `length(y) > 1L`
   if (!length(y) || anyNA(y) || any(!nzchar(y))) stop('Package ', sQuote(x), ' updated?')
   
@@ -58,6 +55,11 @@ bibentry2rmd <- function(x = 'R') {
     }, dots = list(y_ = y, doi_ = doi), MoreArgs = NULL), use.names = FALSE)
   } # [topic](url) for doi, in R markdown grammar
   
-  return(y)
+  return(sprintf(fmt = '<u>**`%s`**</u> %s\n', attr(x, which = 'package', exact = TRUE), y))
+  #return(y)
   
 }
+
+
+
+
