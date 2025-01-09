19a33d4913ffa92dc59addbc3742b1694f1121b2
Tingting Zhan <tingtingzhan@gmail.com>
2025-01-09 09:18:51 -0500

    test

--- R/ma.R
+++ R/ma.R
@@ -16,18 +16,25 @@
 #' 
 #' Function [ma] is much faster than function \link[zoo]{rollmean} in package \CRANpkg{zoo}.
 #' 
+#' Function [ma] imports function \link[stats]{filter} from package \pkg{stats},
+#' not function \link[dplyr]{filter} from package \CRANpkg{dplyr}.
+#' 
+#' @returns
+#' Function [ma] returns a time-series \link[stats]{ts} object from workhorse function \link[stats]{filter}.
+#' 
 #' @examples
 #' unclass(ma(1:20, order = 3L))
-# NOT dplyr::filter
+#' unclass(ma(1:2, order = 3L))
 #' @importFrom stats filter
 #' @export 
 ma <- function(x, order = 5L) {
   nx <- length(x)
   if (nx < order) {
+    # ?stats::filter error
     ret <- rep(NA, times = nx)
     storage.mode(ret) <- storage.mode(x)
     return(ret)
-  }
+  } 
   filter(x = x, filter = rep(1/order, times = order), method = 'convolution', sides = 2)
 }
 
